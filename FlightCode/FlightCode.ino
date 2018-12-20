6a5f81392114fc51089a9db9931b4f202844f0e8
sf314 <sf314@icloud.com>
2018-12-19 18:01:23 -0700

    Add mission config and init flight code

---
+++ FlightCode/FlightCode.ino
@@ -0,0 +1,34 @@
+/**
+ * CanSat 2019 Flight Code
+ * Stephen Flores
+ * 
+ * Responsible for all software-related flight operations
+ * Fully autonomous, with command override capability
+ * Auto-recovery from power loss using SD and/or EEPROM
+ * 
+ * v0.1: Serial heartbeat "Hello, world", unformatted tx packets
+ * 
+ */
+
+// ********** Libraries 
+#include <MissionConfig.h>
+#include <SoftwareSerial.h>
+
+// ********** Global data, i.e. hardware
+SoftwareSerial radio(CS_RADIO_MISO_PIN, CS_RADIO_MOSI_PIN);
+
+
+
+
+// ********** Setup
+void setup() {
+    Serial.begin(9600);
+    radio.begin(9600);
+    
+    pinMode(CS_LED_PIN, OUTPUT);
+}
+
+// ********** Loop
+void loop() {
+    
+}
---
+++ FlightCode/MissionConfig.h
@@ -0,0 +1,22 @@
+/**
+ * Mission Configuration File
+ * Stephen Flores
+ * 
+ * Provide definitions that determine flight software behaviour or configuration
+ * All definitions specific to CanSat will have the `CS_` prefix
+ */
+#ifndef MISSION_CONFIG_H
+#define MISSION_CONFIG_H
+
+// ********** Mission Parameters
+#define CS_DEPLOY_ALT 450
+#define CS_GROUND_ALT_THRESH 50
+
+// ********** Hardware Pinout
+#define CS_RADIO_MISO_PIN 0
+#define CS_RADIO_MOSI_PIN 1
+#define CS_GPS_MISO_PIN 7
+#define CS_GPS_MOSI_PIN 8
+#define CS_LED_PIN 13
+
+#endif
