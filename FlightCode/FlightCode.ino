c0843dbd4e3219b7f90aaabd3179438857a8c2d6
Stephen Flores <sf314@icloud.com>
2019-03-22 14:18:11 -0700

    Add GPS and BME to FlightCode
    It now prints on both USB and radio channels.
    The GPS and BME modules should now be usable from within the flight
    code. Since GPS runs asynchronously, it requires the .update()
    function to be run often.

--- FlightCode/FlightCode.ino
+++ FlightCode/FlightCode.ino
@@ -14,9 +14,18 @@
 #include "MissionConfig.h"
 #include <SoftwareSerial.h>
 
+#include "src/GPS/CSGps.h"
+#include "src/BME/CSBme.h"
+
 // ********** Global data, i.e. hardware
 SoftwareSerial radio(CS_RADIO_MISO_PIN, CS_RADIO_MOSI_PIN);
 SoftwareSerial gpsss(CS_GPS_MISO_PIN, CS_GPS_MOSI_PIN);
+unsigned long currentTime = 0;
+unsigned long prevTime = 0;
+
+// Hardware 
+CSBme bme;
+CSGps gps(&gpsss);
 
 
 
@@ -24,11 +33,36 @@ SoftwareSerial gpsss(CS_GPS_MISO_PIN, CS_GPS_MOSI_PIN);
 void setup() {
     Serial.begin(9600);
     radio.begin(9600);
-    
+    gps.begin(9600);
+
     pinMode(CS_LED_PIN, OUTPUT);
+
+    bme.config();
 }
 
 // ********** Loop
 void loop() {
-    
+    // Update time
+    currentTime = millis();
+
+    // Update GPS and other async functions
+    gps.update();
+
+    if (currentTime - prevTime >= 1000) {
+        prevTime = currentTime;
+
+        Serial.println("USB: alt = " + String(bme.readAlt()));
+        Serial.println("USB: temp = " + String(bme.readTemp()));
+        Serial.println("USB: press = " + String(bme.readPressure()));
+        Serial.println("USB: GPS time = " + String(gps.time));
+        Serial.println();
+
+        radio.println("USB: alt = " + String(bme.readAlt()));
+        radio.println("USB: temp = " + String(bme.readTemp()));
+        radio.println("USB: press = " + String(bme.readPressure()));
+        radio.println("USB: GPS time = " + String(gps.time));
+        radio.println();
+    }
+
+
 }
