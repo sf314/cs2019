dc95b942163f28b100535ee95ca1aa6da7ba246d
Stephen Flores <sf314@icloud.com>
2019-03-22 11:46:33 -0700

    Add temp conversion from previous year's code

--- FlightCode/src/Temp/CSTemp.cpp
+++ FlightCode/src/Temp/CSTemp.cpp
@@ -11,6 +11,18 @@ void CSTemp::config(int p) {
 }
 
 float CSTemp::read() {
-    // Convert 
-    return analogRead(pin);
+    double pinMillivolts = analogRead(pin) * ((double)voltage / 1024);
+    double celsius = (pinMillivolts - 500) / 10; // in celsius
+
+    //double fahrenheit =  celsius * 1.8 + 32;
+    if (celsius < 100.0) {
+        return celsius;
+    } else {
+        // It's whack. Do the mapping thing.
+        // 961->27, 920->23
+        int raw = readRaw();
+        double t1 = 0.09756 * (double)raw;
+        double t2 = 66.756;
+        return t1 - t2;
+    }
 }
