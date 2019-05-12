c5b86f3440da9bb89ff0088eb056df87ddac9d18
Stephen Flores <sf314@icloud.com>
2019-05-12 11:34:12 -0700

    Add functionality to FlightCode

--- FlightCode/FlightCode.ino
+++ FlightCode/FlightCode.ino
@@ -17,19 +17,25 @@
 #include "src/GPS/CSGps.h"
 #include "src/BME/CSBme.h"
 #include "src/Gyro/CSGyro.h"
+#include "src/Telem/CSTelem.h"
 
 // ********** Global data, i.e. hardware
 SoftwareSerial radio(CS_RADIO_MISO_PIN, CS_RADIO_MOSI_PIN);
 SoftwareSerial gpsss(CS_GPS_MISO_PIN, CS_GPS_MOSI_PIN);
-unsigned long currentTime = 0;
-unsigned long prevTime = 0;
+unsigned long currentMS = 0;
+unsigned long prevMS = 0;
+unsigned long startTime = 0; // time (seconds) since epoch
+
 
 // Hardware 
 CSBme bme;
 CSGps gps(&gpsss);
 CSGyro gyro;
+CSTelem telem;
+CSVolt volt;
 
-
+// Function prototypes
+void handleCommands(char c);
 
 // ********** Setup
 void setup() {
@@ -46,32 +52,63 @@ void setup() {
 // ********** Loop
 void loop() {
     // Update time
-    currentTime = millis();
+    currentMS = millis();
+    
+    // Handle commands 
+    if (Serial.available()) {
+        handleCommands(Serial.read());
+    }
+    
+    if (radio.available()) {
+        handleCommands(radio.read());
+    }
 
     // Update GPS and other async functions
     gps.update();
 
-    if (currentTime - prevTime >= 1000) {
-        prevTime = currentTime;
+    if (currentMS - prevMS >= 1000) {
+        
         GyroData_t gyroData = gyro.getData();
 
-        Serial.println("USB: alt = " + String(bme.readAlt()));
-        Serial.println("USB: temp = " + String(bme.readTemp()));
-        Serial.println("USB: press = " + String(bme.readPressure()));
-        Serial.println("USB: GPS time = " + String(gps.time));
-        Serial.println("USB: GPS sats = " + String(gps.satellites));
-        Serial.println("USB: GPS lat = " + String(gps.lat));
-        Serial.println("USB: GPS lon = " + String(gps.lon));
-        Serial.println("USB: Gyro X = " + String(gyroData.x));
-        Serial.println();
-
-        radio.println("Radio: alt = " + String(bme.readAlt()));
-        radio.println("Radio: temp = " + String(bme.readTemp()));
-        radio.println("Radio: press = " + String(bme.readPressure()));
-        radio.println("Radio: GPS time = " + String(gps.time));
-        radio.println("Radio: Gyro X = " + String(gyroData.x));
-        radio.println();
+        telem.teamId = 3623;
+        telem.met = gps.getTime() - startTime;
+        // telem.packetCount; // Does not need to be set manually here
+        telem.altitude = bme.readAlt();
+        telem.pressure = bme.pressure();
+        telem.temp = bme.readTemp();
+        // telem.voltage = volt.read();
+        // telem.gpsTime = gps.timeFormatted();
+        telem.gpsLat = gps.lat;
+        telem.gpsLon = gps.lon;
+        telem.gpsAlt = gps.altitude;
+        telem.gpsSats = gps.satellites;
+        // telem.pitch = gyro.getPitch();
+        // telem.roll = gyro.getRoll();
+        // telem.bladeSpinRate = 
+        // telem.state; // Does not need to be set manually here
+        
+        // Transmit telem over serial and radio
+        Serial.println(telem.toString());
+        radio.println(telem.toString());
+        
+        // Set time for next loop
+        prevMS = currentMS;
     }
 
 
+}
+
+
+void handleCommands(char c) {
+    switch (c) {
+    case 'g':
+        Serial.println("Setting ground height...");
+        break;
+    case 'q':
+        Serial.println("Forcing cut");
+        nichrome.start();
+        break;
+    default:
+        break;
+    }
 }
