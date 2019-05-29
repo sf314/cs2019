e98ab9bbdbb9473304582bab5866abef521a0f59
Stephen Flores <sf314@icloud.com>
2019-05-29 12:00:31 -0700

    Enable SD and Comms for Gyro testing

--- FlightCode/src/Gyro/Gyro.ino
+++ FlightCode/src/Gyro/Gyro.ino
@@ -5,26 +5,40 @@ CSGyro gyro;
 
 
 #define USE_COMMS 1
+#define USE_SD 1
 
 #if USE_COMMS==1
 #include <SoftwareSerial.h>
 SoftwareSerial radio(0, 1);
 #endif
 
+#if USE_SD==1
+#include "CSSDCard.h"
+CSSDCard sdCard;
+#endif
+
 
 void setup() {
+    delay(500);
     Serial.begin(9600);
+    Serial.println("Gyro test");
     
     #if USE_COMMS==1
     radio.begin(9600);
     #endif
     
-    Serial.println("Gyro test");
-    gyro = CSGyro();
+    #if USE_SD==1
+    sdCard.config("datafile.csv");
+    #endif
+    
+    gyro.config();
+    Serial.println("Starting loop...");
 }
 
 void loop() {
+    // Serial.println("Getting data...");
     // GyroData_t gdata = gyro.getData(); // REGULAR TEST DATA
+    // Serial.println("Got data");
     // String dataString;
     // dataString = String(gdata.x) + ", ";
     // dataString += String(gdata.y) + ", ";
@@ -36,6 +50,7 @@ void loop() {
     
     // NEW TEST DATA FOR ACCELERATION
     // Constantly spit out accel data (x/y/z and magnitude)
+    
     AccelData_t a = gyro.getAccelData();
     double mag = sqrt((a.x*a.x) + (a.y*a.y) + (a.z*a.z));
     String telem = 
@@ -49,5 +64,9 @@ void loop() {
     
     #if USE_COMMS==1
     radio.println(telem);
+    #endif
+    
+    #if USE_SD==1
+    sdCard.write(telem);
     #endif
 }
