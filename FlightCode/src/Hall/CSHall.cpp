5f1b4984b5bae8bb2afa43c2cb4cae1eb9aa80cb
Jeremy Lacsa <jlacsa@Jeremys-MacBook-Pro.local>
2019-06-15 03:50:55 -0500

    Changed Hall Effect hitCount structure

--- FlightCode/src/Hall/CSHall.cpp
+++ FlightCode/src/Hall/CSHall.cpp
@@ -9,6 +9,7 @@ volatile unsigned long CSHall_time = 0;
 volatile unsigned long previous2 = 0;
 volatile unsigned long previous1 = 0;
 volatile unsigned long currentTime = 0;
+volatile unsigned long hitCount = 0;
 
 static void update(void);
 
@@ -27,6 +28,10 @@ void CSHall::config(int p) {
 float CSHall::getRpm() {
     // If it's been more than a second since the last ping, decay the value (?) - old
     // This will achieve RPM that is below 60 - old
+
+    // anything less than 60 hits per second is longer than one second
+    // 1 hit every 3 seconds is 20 rpm
+    // 1 hit every 5 seconds is 12 rpm lower bound
     
     // queue of past 3 millis values
     previous2 = previous1;
@@ -59,23 +64,34 @@ float CSHall::getRpm() {
         CSHall_rpm = 0;
     }
     return CSHall_rpm;
+
+    currentTime++;
+
+    if(currentTime % 5 == 0) {
+        currentTime = 0;
+    }
+
 }
 
 static void update() { 
     
-    // Calculate time delta
-    unsigned long current_time = millis();
-    unsigned long delta_time = current_time - CSHall_time;
-    
-    if (delta_time < 200) {
-        return;
-    }
-    
-    // Update RPM value 
-    CSHall_rpm = (1000.0 / delta_time) * 60.0;
-    
-    // Update time
-    CSHall_time = current_time;
-    
-    // Serial.println("interrupt: update RPM @ delt = " + String(delta_time));
+        // // Calculate time delta
+        // unsigned long current_time = millis();
+        // unsigned long delta_time = current_time - CSHall_time;
+        
+        // if (delta_time < 200) {
+        //     return;
+        // }
+        
+        // // Update RPM value 
+        // CSHall_rpm = (1000.0 / delta_time) * 60.0;
+        
+        // // Update time
+        // CSHall_time = current_time;
+        
+        // // Serial.println("interrupt: update RPM @ delt = " + String(delta_time));
+
+    // increment a counter instead
+    hitCount++;
+
 }
